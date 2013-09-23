52323bad99a5bfbfb07bd39c29217f41deda2561
andrew1989 <apridvanturer@gmail.com>
2013-09-23 20:00:38 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turist::Application.config.secret_key_base = '99ec8f3cb99a50a40d0c0af42529074a14f2321809466b441cc81f8be40d586c921e9a45e986652c9720f7f9bb11e88d0d040a4d0100ac18c61cfcbf6c72fe6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turist::Application.config.session_store :cookie_store, key: '_turist_session'
